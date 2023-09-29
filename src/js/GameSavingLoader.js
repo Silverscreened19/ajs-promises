2bc7f55f7c021888529a76fac042348981aef172
Olya Tatianina <olya.tatianina@gmail.com>
2023-09-29 14:09:12 +0400

    initial

---
+++ src/js/GameSavingLoader.js
@@ -0,0 +1,11 @@
+import json from './parser';
+import read from './reader';
+import GameSaving from './GameSaving';
+
+export default class GameSavingLoader {
+  static load() {
+    return read()
+      .then((response) => json(response))
+      .then((response) => new GameSaving(JSON.parse(response)));
+  }
+}
